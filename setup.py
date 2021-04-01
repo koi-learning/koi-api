14a535247ec27428f36557ab6a4bd411276d44e3
Johannes Richter <j.richter@goepel.com>
2021-04-01 09:27:05 +0200

    fixed a dependency issue; sqlalchemy deprecated the Binary field;

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setuptools.setup(
     version="0.1",
     packages=setuptools.find_packages(),
     install_requires=[
+        "sqlalchemy < 1.4.0"
         "flask",
         "flask-cors",
         "flask_sqlalchemy",
