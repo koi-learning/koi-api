12e9e0f1f1e710faf0019eb8fa604df7e5123c93
philipp.drechsler <44899128+philipp79@users.noreply.github.com>
2022-02-23 09:59:27 +0100

    try to fix problem with markupsafe soft_unicode

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setuptools.setup(
     version="0.3.1",
     packages=setuptools.find_packages(),
     install_requires=[
-        "sqlalchemy < 1.4.0",
-        "flask < 2.0.0",
+        "sqlalchemy",
+        "flask",
         "flask-cors",
         "flask_sqlalchemy",
         "flask_restful",
