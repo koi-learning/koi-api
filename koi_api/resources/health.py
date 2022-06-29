df34f133562c830de77576526733ef3d11ef651a
Johannes Richter <hannesrichter@users.noreply.github.com>
2022-06-29 16:10:07 +0200

    added file for health node implementation;

---
+++ koi_api/resources/health.py
@@ -0,0 +1,40 @@
+# Copyright (c) individual contributors.
+# All rights reserved.
+#
+# This is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 3 of
+# the License, or any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details. A copy of the
+# GNU Lesser General Public License is distributed along with this
+# software and can be found at http://www.gnu.org/licenses/lgpl.html
+
+from .base import BaseResource
+from ..common.return_codes import ERR_FORB, SUCCESS
+
+
+class APIHealth(BaseResource):
+    def get(self):
+        return SUCCESS()
+
+    def put(self):
+        """
+        This function nos not allowed.
+        """
+        return ERR_FORB()
+
+    def post(self):
+        """
+        This function nos not allowed.
+        """
+        return ERR_FORB()
+
+    def delete(self):
+        """
+        This function nos not allowed.
+        """
+        return ERR_FORB()
