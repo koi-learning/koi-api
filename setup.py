2f5ecf081af96e33a336846166e1a4da9e9a1fb0
Johannes Richter <hannesrichter@users.noreply.github.com>
2022-08-03 09:50:29 +0200

    New Flask-update breaks our code;

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=[
         "sqlalchemy",
-        "flask",
+        "flask<=2.1.3",
         "flask-cors",
         "flask_sqlalchemy",
         "flask_restful",
